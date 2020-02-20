0029d28006b8f1be07de3391a5e0705f9c2d3914
Danton Cavalcanti Franco Junior <danton.franco@benner.com.br>
2020-02-20 17:49:00 -0300

    Arquivos do vídeo 009 da trilha do WES

---
+++ wes-009-fontes/PessoasController.cs
@@ -0,0 +1,45 @@
+﻿using Benner.Reservas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+/// <summary>
+/// Summary description for PessoasController
+/// </summary>
+public class PessoasController : Controller
+{
+    public PessoasController()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    public ActionResult Salvar([Bind(Include = "txtNome, txtCpf, selPais")] DadosPessoa dadosPessoa)
+    {
+        Pessoas pessoa = Pessoas.Create();
+        pessoa.Nome = dadosPessoa.TxtNome;
+        pessoa.Cpf = dadosPessoa.TxtCpf;
+        pessoa.PaisHandle = dadosPessoa.SelPais;
+        pessoa.Save();
+        ViewBag.Paises = Paises.GetAll();
+        ViewBag.Pessoas = Pessoas.GetAll();
+        return View("Index", ViewBag);
+    }
+
+    public ActionResult Index()
+    {
+        ViewBag.Paises = Paises.GetAll();
+        ViewBag.Pessoas = Pessoas.GetAll();
+        return View(ViewBag);
+    }
+
+}
+
+public class DadosPessoa
+{
+    public string TxtNome { get; set; }
+    public string TxtCpf { get; set; }
+    public int SelPais { get; set; }
+}
