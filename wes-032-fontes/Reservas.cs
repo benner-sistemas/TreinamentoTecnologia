856f31d41b9201b31c49c3a6c24014a4e5f00d8f
Danton Cavalcanti Franco Junior <danton.franco@benner.com.br>
2020-04-13 18:04:50 -0300

    Arquivos do vídeo 032 da trilha do WES

---
+++ wes-032-fontes/Reservas.cs
@@ -0,0 +1,47 @@
+﻿using Benner.Tecnologia.Business;
+using Benner.Tecnologia.Common;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+
+
+namespace Benner.Reservas.Entidades
+{
+    
+    
+    /// <summary>
+    /// Nome da Tabela: RESERVAS.
+    /// Essa é uma classe parcial, os atributos, herança e propriedades estão definidos no arquivo Reservas.properties.cs
+    /// </summary>
+    public partial class Reservas
+    {
+        protected override void Saving()
+        {
+            string mensagem = "Esta reserva custará R$ ";
+
+            if (this.PlanoInstance.Tipo == PlanosTipoListaItens.ItemKilometragem)
+            {
+                if (this.Kilometragem != null && this.Kilometragem.Value > 0m)
+                {
+                    mensagem += this.PlanoInstance.ValorReferencia.Value * this.Kilometragem;
+                }
+            }
+            else if (this.PlanoInstance.Tipo == PlanosTipoListaItens.ItemDiaria)
+            {
+                int numeroDias = (this.DataFim - this.DataInicio).Value.Days;
+                mensagem += this.PlanoInstance.ValorReferencia * numeroDias;
+            }
+            else if (this.PlanoInstance.Tipo == PlanosTipoListaItens.ItemValorFechado)
+            {
+                mensagem += this.PlanoInstance.ValorReferencia;
+            }
+            this.Observacoes = mensagem;
+            base.Saving();
+        }
+    }
+}
