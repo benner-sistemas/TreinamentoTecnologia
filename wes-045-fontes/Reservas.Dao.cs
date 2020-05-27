fa7c6f7f4473d35bc17cf3ea76243c7df174f37a
Danton Cavalcanti Franco Junior <danton.franco@benner.com.br>
2020-05-27 17:42:42 -0300

    Arquivos do vídeo 045 da trilha do WES

---
+++ wes-045-fontes/Reservas.Dao.cs
@@ -0,0 +1,27 @@
+﻿using Benner.Tecnologia.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benner.Reservas.Entidades
+{
+    public partial interface IReservasDao
+    {
+        bool ExisteReservaAgora(Handle carro);
+    }
+    public partial class ReservasDao : IReservasDao
+    {
+        public bool ExisteReservaAgora(Handle carro)
+        {
+            bool result = false;
+            Criteria criteria = new Criteria(@"A.CARRO = :CARRO
+                                                    AND
+                                              @HOJE BETWEEN A.DATAINICIO AND A.DATAFIM");
+            criteria.Parameters.Add("CARRO", carro);
+            result = GetMany(criteria).Count > 0;
+            return result;
+        }
+    }
+}
