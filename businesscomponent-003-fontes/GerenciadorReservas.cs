318226219d9f8680ea0c495425dfb558a998cc0e
Danton Cavalcanti Franco Junior <danton.franco@benner.com.br>
2020-07-09 16:33:12 -0300

    Arquivos do vídeo 003 da trilha de BusinessComponent

---
+++ businesscomponent-003-fontes/GerenciadorReservas.cs
@@ -0,0 +1,18 @@
+﻿using Benner.Reservas.Entidades;
+using Benner.Reservas.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benner.Reservas.Componentes
+{
+    public class GerenciadorReservas : IGerenciadorReservas
+    {
+        public string AprovarReserva(IReservas reserva)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ businesscomponent-003-fontes/IGerenciadorReservas.cs
@@ -0,0 +1,14 @@
+﻿using Benner.Reservas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benner.Reservas.Interfaces
+{
+    public interface IGerenciadorReservas
+    {
+        string AprovarReserva(IReservas reserva);
+    }
+}
