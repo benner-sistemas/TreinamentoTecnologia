c109bb9d48c9763c2e0f9eae5770d1de27a62e9a
Danton Cavalcanti Franco Junior <danton.franco@benner.com.br>
2020-07-13 08:58:30 -0300

    Arquivos do vídeo 004 da trilha de BusinessComponent

---
+++ businesscomponent-004-fontes/GerenciadorReservas.cs
@@ -0,0 +1,26 @@
+﻿using Benner.Reservas.Entidades;
+using Benner.Reservas.Interfaces;
+using Benner.Tecnologia.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benner.Reservas.Componentes
+{
+    public class GerenciadorReservas : IGerenciadorReservas
+    {
+        public string AprovarReserva(IReservas reserva)
+        {
+            string mensagem = string.Empty;
+
+            if (reserva.Status != ReservasStatusListaItens.ItemAguardandoAprovacao)
+            {
+                throw new BusinessException("Esta reserva não está aguardando aprovação!");
+            }
+
+            return mensagem;
+        }
+    }
+}
