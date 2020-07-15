163b216a86912202681b7e4052e5b818581d2328
Danton Cavalcanti Franco Junior <danton.franco@benner.com.br>
2020-07-15 17:56:10 -0300

    Arquivos do vídeo 006 da trilha de BusinessComponent

---
+++ businesscomponent-006-fontes/Núcleo/RegistradorDependencias.cs
@@ -0,0 +1,20 @@
+﻿using Benner.Reservas.Componentes;
+using Benner.Reservas.Interfaces;
+using Benner.Tecnologia.Business;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benner.Reservas.Nucleo.IoC
+{
+    public class RegistradorDependencias : NinjectModule
+    {
+        public override void Load()
+        {
+            BusinessComponent.Register<IGerenciadorReservas, GerenciadorReservas>(Kernel);
+        }
+    }
+}
---
+++ businesscomponent-006-fontes/WES/RegistradorDependencias.cs
@@ -0,0 +1,19 @@
+﻿using Benner.Reservas.Interfaces;
+using Benner.Tecnologia.Business;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benner.Reservas.Wes.Nucleo.IoC
+{
+    public class RegistradorDependencias : NinjectModule
+    {
+        public override void Load()
+        {
+            BusinessComponent.RegisterProxy<IGerenciadorReservas>(Kernel);
+        }
+    }
+}
