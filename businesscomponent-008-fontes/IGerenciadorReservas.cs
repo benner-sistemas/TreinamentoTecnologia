7b3f8039dd498931b9d2fde8ee125be014f9babc
Danton Cavalcanti Franco Junior <danton.franco@benner.com.br>
2020-07-17 17:59:00 -0300

    Arquivos do vídeo 008 da trilha de BusinessComponent

---
+++ businesscomponent-008-fontes/IGerenciadorReservas.cs
@@ -0,0 +1,18 @@
+﻿using Benner.Reservas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benner.Reservas.Interfaces
+{
+    public interface IGerenciadorReservas
+    {
+        string AprovarReserva(IReservas reserva);
+
+        string DevolverReserva(IReservas reserva);
+
+        string RecusarReserva(IReservas reserva);
+    }
+}
