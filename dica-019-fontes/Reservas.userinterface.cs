c135096719f63dd483d4a9b076f68c5f5fb9a5bd
Danton Cavalcanti Franco Junior <danton.franco@benner.com.br>
2020-07-21 18:09:56 -0300

    Arquivos do vídeo 019 da trilha de Dicas

---
+++ dica-019-fontes/Reservas.userinterface.cs
@@ -0,0 +1,35 @@
+﻿using Benner.Tecnologia.Common.Scripting.UserInterface;
+using System;
+
+namespace Benner.Reservas.Entidades
+{
+    [ScriptUI()]
+    public partial class Reservas
+    {
+        [ViewLoaded]
+        public void ViewLoaded()
+        {
+            VerificaDataSolicitacao();
+            MostrarQuilometragem();
+        }
+
+        public void VerificaDataSolicitacao()
+        {
+            if (this.DataSolicitacao != null && this.DataSolicitacao.Value.AddDays(7) < DateTime.Now)
+            {
+                this[FieldNames.DataSolicitacao].Label = "Data de solicitação (mais 7 dias)";
+            }
+        }
+
+        [FieldChanged(FieldNames.Plano)]
+        public void PlanoChange()
+        {
+            MostrarQuilometragem();
+        }
+
+        private void MostrarQuilometragem()
+        {
+            this[FieldNames.Quilometragem].Visible = (this[FieldNames.Plano].GetInt32() == PlanosTipoListaItens.ItemKilometragem);
+        }
+    }
+}
