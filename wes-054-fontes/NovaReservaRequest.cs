ba7ecc42d471f70cf4c8497defcf28db71eee9a1
Danton Cavalcanti Franco Junior <danton.franco@benner.com.br>
2020-08-13 18:11:32 -0300

    Arquivos do vídeo 054 da trilha do WES

---
+++ wes-054-fontes/NovaReservaRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benner.Reservas.Comum
+{
+    public class NovaReservaRequest
+    {
+        public long HandlePessoa { get; set; }
+        public long HandleModeloCarro { get; set; }
+        public long HandlePlano { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFim { get; set; }
+    }
+}
---
+++ wes-054-fontes/NovaReservaResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benner.Reservas.Comum
+{
+    public class NovaReservaResponse
+    {
+        public long HandleReservaCriada { get; set; }
+    }
+}
