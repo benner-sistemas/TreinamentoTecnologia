64d7ccd75879b267c5c09e4f5b559c82af032b7a
Danton Cavalcanti Franco Junior <danton.franco@benner.com.br>
2020-08-20 16:15:39 -0300

    Arquivos do vídeo 056 da trilha do WES

---
+++ wes-056-fontes/Pessoas.Dao.cs
@@ -0,0 +1,43 @@
+﻿using Benner.Tecnologia.Business;
+using Benner.Tecnologia.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benner.Reservas.Entidades
+{
+    public partial interface IPessoasDao
+    {
+        Handle CriarPessoaSemRegraNegocio(string nome, string cpf, string email, string telefoneMovel);
+    }
+    public partial class PessoasDao : IPessoasDao
+    {
+        public Handle CriarPessoaSemRegraNegocio(string nome, string cpf, string email, string telefoneMovel)
+        {
+            Handle handle = null;
+            using (var tc = new TransactionContext())
+            {
+                handle = Pessoas.NovoHandle();
+
+                RawEntityCommand command =
+                    new RawEntityCommand(@"INSERT INTO PESSOAS
+                                                (HANDLE, NOME, CPF, EMAIL, TELEFONEMOVEL) 
+                                           VALUES
+                                                (:HANDLE, :NOME, :CPF, :EMAIL, :TELEFONEMOVEL)
+                                          ");
+                command.Parameters.Add("HANDLE", handle);
+                command.Parameters.Add("NOME", nome);
+                command.Parameters.Add("CPF", cpf);
+                command.Parameters.Add("EMAIL", email);
+                command.Parameters.Add("TELEFONEMOVEL", telefoneMovel);
+
+                Entity.Execute(command);
+
+                tc.Complete();
+            }
+            return handle;
+        }
+    }
+}
---
+++ wes-056-fontes/Pessoas.cs
@@ -0,0 +1,53 @@
+﻿using Benner.Tecnologia.Business;
+using Benner.Tecnologia.Business.Validation;
+using Benner.Tecnologia.Common;
+using Microsoft.Practices.EnterpriseLibrary.Validation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+
+
+namespace Benner.Reservas.Entidades
+{
+
+
+    /// <summary>
+    /// Nome da Tabela: PESSOAS.
+    /// Essa é uma classe parcial, os atributos, herança e propriedades estão definidos no arquivo Pessoas.properties.cs
+    /// </summary>
+    public partial class Pessoas
+    {
+        public static Handle NovoHandle()
+        {
+            return NewHandle();
+        }
+
+        protected override void Validating()
+        {
+            if (this.DataAlteracao == null)
+            {
+                this.DataAlteracao = DateTime.Now;
+            }
+
+            if (this.DataCriacao == null)
+            {
+                this.DataCriacao = DateTime.Now;
+            }
+            base.Validating();
+        }
+
+        public override void Validate(ValidationResults validationResults)
+        {
+            if (!this.Cpf.IsValidCPF())
+            {
+                validationResults.AddResult(new EntityValidationResult("CPF é inválido!!!"));
+            }
+            base.Validate(validationResults);
+        }
+    }
+}
