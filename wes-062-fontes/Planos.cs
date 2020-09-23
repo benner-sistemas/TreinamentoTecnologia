b2b72c49733ed960c985591e4639e02ba2a83157
Danton Cavalcanti Franco Junior <danton.franco@benner.com.br>
2020-09-23 17:04:19 -0300

    Arquivos do vídeo 062 da trilha do WES

---
+++ wes-062-fontes/Planos.cs
@@ -0,0 +1,35 @@
+﻿using Benner.Tecnologia.Business;
+using Benner.Tecnologia.Common;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+
+
+namespace Benner.Reservas.Entidades
+{
+    
+    
+    /// <summary>
+    /// Nome da Tabela: PLANOS.
+    /// Essa é uma classe parcial, os atributos, herança e propriedades estão definidos no arquivo Planos.properties.cs
+    /// </summary>
+    public partial class Planos
+    {
+        public static void AjustaValorReferencia(BusinessArgs args)
+        {
+            var planos = Planos.GetAll();
+            foreach (var plano in planos)
+            {
+                var valorAtual = plano.ValorReferencia;
+                plano.ValorReferencia = valorAtual + (valorAtual * Convert.ToDecimal(args.DataEntity.Fields["PERCENTUAL"]) / 100);
+                plano.Save();
+            }
+            args.Message = "Atualização bem sucedida!!!";
+        }
+    }
+}
